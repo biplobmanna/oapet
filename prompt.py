e09eecf737217c07c0fa6ad334ab1598feaca94f
biplobmanna <biplobmanna@outlook.com>
2021-01-04 19:03:38 +0530

    chore: all your prompts are here

---
+++ prompt.py
@@ -0,0 +1,27 @@
+# Import the search function to get the list of all the files
+from search import search_projects
+
+def show_search_prompt():
+    # Any other ideas for minimal-cool-project
+    return input('?: ')
+
+def choose_options_prompt(projects):
+    # Uff, a new line only, eh?
+    print()
+    index = 1
+    for project in projects:
+        print("( " + str(index) + ".) " + project)
+        index += 1
+    try:
+        choice = int(input("\nChoose: "))
+        # Sanity check on choice
+        if choice >= index or choice <= 0 :
+            raise("Foolish choice lad...")
+    except:
+        # Don't you dare supply rubbish
+        print("\nFoolish choice lad...\n")
+        exit(0)
+    return choice
+
+
+# choose_options_prompt(search_projects('home'))
