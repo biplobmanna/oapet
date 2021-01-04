7598f50581bfa525fe8ae3534ae0b58adb62ddcc
biplobmanna <biplobmanna@outlook.com>
2021-01-04 09:25:16 +0530

    chore: search - fuzzy search all the folders to matching name

---
+++ search.py
@@ -0,0 +1,43 @@
+# Get the project_path
+from config_manager import PROJECT_PATH
+
+# READ only folders in Project Directory
+from os import listdir
+from os.path import isdir, join
+projects = [
+    name 
+    for name in listdir(PROJECT_PATH) 
+        if isdir(join(PROJECT_PATH, name))
+]
+
+# Setup Fuzzy Search
+from fuzzysearch import find_near_matches as fnm
+
+
+def search_projects(search_str=""):
+    """
+        return matched project names sorted by match-level
+        if no search input given
+        return all names
+    """
+    # if empty search string, return all
+    if not search_str:
+        return projects 
+    # List which will contain the matches -- 2D list
+    # ['matched_project_name', 'len_of_matched_substr']
+    near_matches = []
+
+    # iterate through all the folders & apply fuzzy search
+    for project in projects:
+        matched_result = fnm(search_str, project, max_l_dist=1)
+        if matched_result:
+            near_matches.append([project, len(matched_result[0].matched)])
+    
+    near_matches.sort(reverse=True, key=lambda matches: matches[1])
+    return [matches[0] for matches in near_matches]
+
+
+# print(near_matches_df)
+# print(near_matches)
+# near_matches.sort(reverse=True, key=lambda matches: matches[1])
+print(search_projects("home"))
