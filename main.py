7569eca2daa2fc6e5448866b43c35d6e9d3c9580
biplobmanna <biplobmanna@outlook.com>
2021-01-05 09:24:51 +0530

    chore: modified main - joining input from args

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import search
 import open_editor
 
 # Start with displaying a prompt to take input
-search_str = prompt.show_search_prompt()
+# search_str = prompt.show_search_prompt()
+
+# Take arguments from the command line instead
+import sys
+arg_list = sys.argv
+arg_list.pop(0)
+arg_list_str = ""
+for arg in arg_list:
+    # print(arg)
+    arg_list_str = arg_list_str + arg
+search_str = arg_list_str.strip()
 
 # Perform Fuzzy Search & get the list of matches
 search_matches = search.search_projects(search_str)
